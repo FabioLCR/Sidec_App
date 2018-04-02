38a624838a0d55582b57a57d8c7730e8038c7f4b
Fabio Ribeiro <fribeiro@img.com.br>
2018-04-02 13:21:21 -0300

    routing

--- Sidec/src/app/app.module.ts
+++ Sidec/src/app/app.module.ts
@@ -1,23 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { KeycloakService, KeycloakAngularModule } from 'keycloak-angular';
 import { initializer } from './app-init';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { InboxComponent } from './inbox/inbox.component';
+import { NovaSolicitacaoComponent } from './nova-solicitacao/nova-solicitacao.component';
+import { PesquisaComponent } from './pesquisa/pesquisa.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    InboxComponent,
+    NovaSolicitacaoComponent,
+    PesquisaComponent,
+    PageNotFoundComponent,
   ],
   imports: [
-    RouterModule.forRoot([
-      {
-        path: '**',
-        component: AppComponent
-      }
-    ], { useHash: true }),
     BrowserModule,
+    AppRoutingModule,
     KeycloakAngularModule
   ],
   providers: [
