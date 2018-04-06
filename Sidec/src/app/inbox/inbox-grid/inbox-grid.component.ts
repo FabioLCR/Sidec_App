9394b00c20bd392bd0ab34e8db69b6097bdd0309
Fabio Ribeiro <fribeiro@img.com.br>
2018-04-06 14:36:14 -0300

    grid 1

--- Sidec/src/app/inbox/inbox-grid/inbox-grid.component.ts
+++ Sidec/src/app/inbox/inbox-grid/inbox-grid.component.ts
@@ -1,33 +1,52 @@
 import { Component, ViewEncapsulation, OnInit } from '@angular/core';
+import { MockServerResultsService } from '../../services/inbox/mock-server-results.service';
+import { Page } from '../../services/inbox/model/page';
+import { CorporateEmployee } from '../../services/inbox/model/corporate-employee';
 
 @Component({
   selector: 'app-inbox-grid',
+  providers: [
+      MockServerResultsService
+  ],
   templateUrl: './inbox-grid.component.html',
   encapsulation: ViewEncapsulation.None,
   styleUrls: ['./inbox-grid.component.css']
 })
 export class InboxGridComponent implements OnInit {
 
-  // rows = [];
-  // loadingIndicator: boolean = true;
-  // reorderable: boolean = true;
+  page = new Page();
+  rows = new Array<CorporateEmployee>();
 
-  // columns = [
-  //   { prop: 'name' },
-  //   { name: 'Gender' },
-  //   { name: 'Company' }
-  // ];
+  //Para ServerSide é usado um serviço no construtor
+  constructor(private serverResultsService: MockServerResultsService) {
+    this.page.pageNumber = 0;
+    this.page.size = 10;
+    //Client Side
+    // this.fetch((data) => {
+    //   this.rows = data
+    // });
+  }
 
-  // constructor() {
-  //   this.fetch((data) => {
-  //     this.rows = data;
-  //     setTimeout(() => { this.loadingIndicator = false; }, 1500);
-  //   });
-  // }
+  ngOnInit() {
+    this.setPage({ offset: 0 });
+  }
 
+  /**
+   * Populate the table with new data based on the page number
+   * @param page The page to select
+   */
+  setPage(pageInfo){
+    this.page.pageNumber = pageInfo.offset;
+    this.serverResultsService.getResults(this.page).subscribe(pagedData => {
+      this.page = pagedData.page;
+      this.rows = pagedData.data;
+    });
+  }
+
+  // Esta responsabilidade foi para o serviço
   // fetch(cb) {
   //   const req = new XMLHttpRequest();
-  //   req.open('GET', 'assets/data/company.json');
+  //   req.open('GET', `assets/data/company.json`);
 
   //   req.onload = () => {
   //     cb(JSON.parse(req.response));
@@ -36,28 +55,6 @@ export class InboxGridComponent implements OnInit {
   //   req.send();
   // }
 
-  rows = [];
-  joke = 'knock knock';
-  currentIndex = 0;
-
-  constructor() {
-    this.fetch((data) => {
-      this.rows = data
-    });
-  }
-
-  fetch(cb) {
-    const req = new XMLHttpRequest();
-    req.open('GET', `assets/data/company.json`);
-
-    req.onload = () => {
-      cb(JSON.parse(req.response));
-    };
-
-    req.send();
-  }
-
-  ngOnInit() {
-  }
+  
 
 }
