fb9661e18614444f99bcbd76a845c8dacbbb58ad
Fábio Luiz Cortez Ribeiro <34350928+FabioLCR@users.noreply.github.com>
2018-05-11 08:15:13 -0300

    filtro completo

--- Sidec/src/app/services/model/filter-inbox-data.ts
+++ Sidec/src/app/services/model/filter-inbox-data.ts
@@ -19,8 +19,7 @@ export class FilterInboxData {
         if (agente != null && agente != 'Todos') {
             this.agente = agente;
         }
-        else
-        {
+        else {
             this.agente = null;
         }
     }
@@ -29,18 +28,16 @@ export class FilterInboxData {
         if (etapa != null && etapa != 'Todas') {
             this.etapa = etapa;
         }
-        else
-        {
+        else {
             this.etapa = null;
         }
     }
 
     public setData(data: NgbDateStruct) {
         if (data != null) {
-            this.data = `${data.year}-${data.month}-${data.day}`
+            this.data = `${data.year}-${this.pad(data.month, 2)}-${data.day}`
         }
-        else
-        {
+        else {
             this.data = null;
         }
     }
@@ -49,8 +46,7 @@ export class FilterInboxData {
         if (texto.trim() != null) {
             this.texto = texto;
         }
-        else
-        {
+        else {
             this.texto = null;
         }
     }
@@ -63,22 +59,150 @@ export class FilterInboxData {
             if (this.agente != '') {
                 this.where += "(tx_respetapa = '" + this.agente + "')";
             }
-            
+
             if (this.etapa != '') {
                 if (this.where !== '(') {
                     this.where += ' And '
                 }
-                this.where += '(li_etapa = ' + this.etapa + ')';                
+                this.where += '(li_etapa = ' + this.etapa + ')';
             }
-            
+
             if (this.data != null) {
                 if (this.where !== '(') {
                     this.where += ' And '
                 }
-                this.where += "(dt_dtinbox = '" + this.data + "')";                
+                this.where += "(dt_dtinbox LIKE '%" + this.data + "%')";
             }
+            this.where += ')';
+        }
+
+        //segunda parte: Geral
+        if (this.texto != '') {
+            this.texto = this.texto.toUpperCase();
+
+            if (this.where !== '1=1') {
+                this.where += ' And '
+            }
+            else {
+                this.where = '';
+            }
+            this.where += '(';
+            //Solicitação
+            this.where += "(UPPER(li_nsolicitacao) LIKE '%" + this.texto + "%')";
+
+            //Data
+
+            var _texto: string = this.texto.replace(/\//g, '-');
+            var _date: string[] = _texto.split('-');
+
+            //_texto = _texto.replace(/(\w\w)\-(\w\w)\-(\w\w\w\w)(.*)/, '$3-$2-$1$4')
+
+            var _texto: string;
+            switch (_date.length) {
+                case 2:
+                    _texto = _texto.replace(/(\w*)\-(\w*)/, '$2-$1');
+                    break;
+                case 3:
+                    _texto = _texto.replace(/(\w*)\-(\w*)\-(\w*)/, '$3-$2-$1');
+                    break;
+                default:
+                    if (_texto.match('.') != null) {
+                        _texto = '';
+                    }
+                    break;
+            }
+            if (_texto !== '') {
+                this.where += ' Or '
+                this.where += "(dt_dtinbox LIKE '%" + _texto + "%')";
+            }
+            //COBRADE
+            var lista: number[] = [];
+            for (let index = 0; index < SidecDomains.DC_COBRADE.length; index++) {
+                const _cobrade = SidecDomains.DC_COBRADE[index];
+                if (_cobrade.name.toUpperCase().match(this.texto) != null) {
+                    lista.push(_cobrade.code);
+                }
+            };
+
+            for (let index = 0; index < lista.length; index++) {
+                if (index === 0) {
+                    this.where += ' Or '
+                    this.where += "(li_cobrade IN(";
+
+                    this.where += lista[index];
+                }
+                else {
+                    this.where += ', ' + lista[index];
+                }
+            }
+            if (lista.length > 0) {
+                this.where += "))";
+            }
+
+            //Motivo alegado
+            this.where += ' Or '
+            this.where += "(UPPER(tx_motalegado) LIKE '%" + this.texto + "%')";
+
+            //Solicitação
+            this.where += ' Or '
+            this.where += "(li_nsolicitacao LIKE '%" + this.texto + "%')";
+
+            //Situação
+            lista = [];
+            for (let index = 0; index < SidecDomains.DC_SITUACAO.length; index++) {
+                const _situacao = SidecDomains.DC_SITUACAO[index];
+                if (_situacao.name.toUpperCase().match(this.texto) != null) {
+                    lista.push(_situacao.code);
+                }
+            };
+
+            for (let index = 0; index < lista.length; index++) {
+                if (index === 0) {
+                    this.where += ' Or '
+                    this.where += "(li_situacao IN(";
+
+                    this.where += lista[index];
+                }
+                else {
+                    this.where += ', ' + lista[index];
+                }
+            }
+            if (lista.length > 0) {
+                this.where += "))";
+            }
+            
+            //ETAPA
+            lista = [];
+            for (let index = 0; index < SidecDomains.DC_ETAPA_CHAMADO.length; index++) {
+                const _etapa = SidecDomains.DC_ETAPA_CHAMADO[index];
+                if (_etapa.name.toUpperCase().match(this.texto) != null) {
+                    lista.push(_etapa.code);
+                }
+            };
+
+            for (let index = 0; index < lista.length; index++) {
+                if (index === 0) {
+                    this.where += ' Or '
+                    this.where += "(li_etapa IN(";
+
+                    this.where += lista[index];
+                }
+                else {
+                    this.where += ', ' + lista[index];
+                }
+            }
+            if (lista.length > 0) {
+                this.where += "))";
+            }
+
             this.where += ')';
         }
         return this.where;
     }
+
+    pad(n, len): string {
+        var num = parseInt(n, 10);
+        len = parseInt(len, 10);
+        return (isNaN(num) || isNaN(len)) ? n : (1e10 + "" + num).slice(-len);
+    };
 }
